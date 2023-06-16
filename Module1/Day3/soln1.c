40ced702ad8fbc47f8e9234e2c6159f7eb4d09df
priyadharshini <priyalaha3082001@gmail.com>
2023-06-16 22:53:46 +0530

    commit 11 : main Added Module1/Day3/

--- Module1/Day3/soln1.c
+++ Module1/Day3/soln1.c
@@ -1,5 +1,23 @@
-//Patern Generator:
-#include<stdio.h>
-int main(){
-    printf("hi");
+//print bits
+
+#include <stdio.h>
+void printBits(unsigned int num) 
+{
+    int numBits = sizeof(num) * 8;
+    for (int i = numBits - 1; i >= 0; i--) {
+        int bit = (num >> i) & 1;
+        printf("%d", bit);
+        if (i % 8 == 0) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
+int main() {
+    unsigned int num;
+    printf("Enter a 32-bit integer: ");
+    scanf("%u", &num);
+    printf("Bits: ");
+    printBits(num);
+    return 0;
 }
